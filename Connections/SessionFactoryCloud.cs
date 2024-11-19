61c18f2d380ace6b96b523641c7e400a083f2490
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-11-19 15:05:45 +0100

    Deleted the examples provided by dotnet

--- Connections/SessionFactoryCloud.cs
+++ Connections/SessionFactoryCloud.cs
@@ -11,7 +11,7 @@ namespace cat.itb.M6UF2EA3.connections
         private static string ConnectionString = "Server=tyke.db.elephantsql.com;Port=5432;Database=ipnnzuuj;User Id=ipnnzuuj;Password=z-VCsCRQ6Xo8S3aVf6UKX7xSjCOKKVJR;";
         private static ISessionFactory session;
 
-        public static ISessionFactory CreateSession<T>()
+        private static ISessionFactory CreateSession<T>()
         {
             if (session != null)
                 return session;
