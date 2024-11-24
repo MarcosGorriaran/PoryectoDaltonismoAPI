d46fc184242626acb8e75305e16ba43b7ca24a5a
MarcosGorriaran <146812640+MarcosGorriaran@users.noreply.github.com>
2024-11-24 18:00:20 +0100

    Made the session controller

--- Controllers/SessionAPIController.cs
+++ Controllers/SessionAPIController.cs
@@ -2,9 +2,12 @@
 using APIDaltonismoDB.Model;
 using cat.itb.M6UF2Pr;
 using Microsoft.AspNetCore.Mvc;
+using ColorBlindProyect_Api;
+using Microsoft.AspNetCore.Cors;
 
 namespace APIDaltonismoDB.Controllers
 {
+    [EnableCors(Program.CORSPolicyName)]
     public class SessionAPIController
     {
         private ResponseDTO _response;
@@ -24,6 +27,34 @@ namespace APIDaltonismoDB.Controllers
             {
                 _response.Data = _dbSession.SelectAll();
             }catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpGet("GetScore/{id}")]
+        public ResponseDTO GetScore(string id)
+        {
+            try
+            {
+                _response.Data = _dbSession.SelectById(id);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess=false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPost("AddScore")]
+        public ResponseDTO AddScore([FromBody] Session session)
+        {
+            try
+            {
+                _dbSession.Insert(session);
+            }
+            catch (Exception ex)
             {
                 _response.IsSuccess = false;
                 _response.Message = ex.Message;
